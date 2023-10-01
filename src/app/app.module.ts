2610c9ee9a165a7f942cccac643f85b53f7b1fde
Nina Wiedrich <94755570+ninawiedrich@users.noreply.github.com>
2023-10-01 20:45:01 +0200

    integrated user registration and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Importing the Angular Material modules after installing them
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationFormComponent,
+    UserLoginFormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule, // Add HttpClientModule to the imports array
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// This import will be used to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in the fetch-api-data.service.ts file
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() loginData = { username: '', password: '' };
+
+constructor	(
+  public fetchApiData: FetchApiDataService,
+  public dialogRef: MatDialogRef<UserLoginFormComponent>,
+  public snackBar: MatSnackBar) { }
+
+ngOnInit(): void {
+}
+
+loginUser(): void {
+  this.fetchApiData.userLogin(this.loginData).subscribe((result) => {
+    // Logic for a successful user login goes here! (To be implemented)
+    localStorage.setItem('user', JSON.stringify(result.user));//result.user.Username   --instead JSON.stringify?
+    localStorage.setItem('token', result.token);
+    
+    this.dialogRef.close(); // This will close the modal on success!
+    this.snackBar.open('Logged in', 'OK', {
+      duration: 2000
+    });
+  }, (result) => {
+    this.snackBar.open(result, 'OK', {
+      duration: 2000
+    });
+  });
+}
+
+}
+
+
